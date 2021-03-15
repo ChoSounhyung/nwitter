3c2ce4d319532aa6d065e226706c6d4a110ac450
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2021-03-16 05:05:47 +0900

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+// web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxuSjV2pwTsWdkIukvvrsRoqwzNTFC-e4",
+  authDomain: "nwitter-699a0.firebaseapp.com",
+  projectId: "nwitter-699a0",
+  storageBucket: "nwitter-699a0.appspot.com",
+  messagingSenderId: "90458361090",
+  appId: "1:90458361090:web:3b8fc9aa0cc4ba1b48061b",
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
