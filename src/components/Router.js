f819afcb21c1e38cc0cc3a000b6081deeeda020c
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2021-03-18 00:38:47 +0900

    Router Setup

---
+++ src/components/Router.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+  //render시킬 routes는 로그인 여부에 따라 달라짐
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <Router>
+      <Switch>
+        {/* 사용자가 로그인 되어있는 상태 : isLoggedIn */}
+        {isLoggedIn ? (
+          <>
+            {" "}
+            {/* fragment : 많은 요소들를 render하고 싶을 때(부모 요소가 없을 때), div, span에 넣기 싫을 때 */}
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}{" "}
+        {/* Auth : 로그인 페이지 */}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
---
+++ src/routes/Auth.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+const Auth = () => <span>Auth</span>;
+export default Auth; //  자동 improt 되게 해줌
