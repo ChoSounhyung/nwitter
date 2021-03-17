2f0b376142a966affd4aebdb9da40781057fa25a
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2021-03-18 02:55:40 +0900

    Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
   //render시킬 routes는 로그인 여부에 따라 달라짐
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {/* 사용자가 로그인 되어있는 상태 : isLoggedIn */}
         {isLoggedIn ? (
           <>
-            {" "}
             {/* fragment : 많은 요소들를 render하고 싶을 때(부모 요소가 없을 때), div, span에 넣기 싫을 때 */}
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+            {/* <Redirect from="*" to="/" /> */}
+            {/* "/" route에 있으면 상관 없음, 그 외의 route로 가게되면 "/"로 돌아가라는 의미 */}
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
-        )}{" "}
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+            {/* <Redirect from="*" to="/" /> */}
+          </>
+        )}
         {/* Auth : 로그인 페이지 */}
       </Switch>
     </Router>
